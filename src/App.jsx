3000bcac75ab47af60a4a48a09d90caf06e38420
John Pascual <ashleypascual@hotmail.es>
2024-09-07 17:31:07 +0100

    style contact component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Contact from "./components/Contact/Contact";
 
 const App = () => {
   return (
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import classes from "./Contact.module.css";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className={`${classes.container}`}>
+        <div className={`${classes.innerContainer}`}>
+          <h2 className={`${classes.contact}`}>Contact</h2>
+          <p className={`${classes.contactParagraph}`}>
+            I would love to hear about your project and how I could help. Please
+            fill in the form, and I'll get back to you as soon as possible.
+          </p>
+        </div>
+        <div>
+          <form action="post">
+            <div className={`${classes.formContainer}`}>
+              <div>
+                <input
+                  type="text"
+                  className={`${classes.input}`}
+                  placeholder="NAME"
+                />
+                <hr />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  name=""
+                  id=""
+                  className={`${classes.input}`}
+                  placeholder="EMAIL"
+                />
+                <hr />
+              </div>
+              <div>
+                <textarea
+                  className={`${classes.input} ${classes.textarea}`}
+                  placeholder="MESSAGE"
+                />
+                <hr />
+              </div>
+              <div className={`${classes.buttonContainer}`}>
+                <button type="submit" className={`${classes.button}`}>
+                  SEND MESSAGE
+                  <hr />
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
