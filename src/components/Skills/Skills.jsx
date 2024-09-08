27b6eec6543ed7e4d93c1f3b10356ea0fa8c0977
John Pascual <ashleypascual@hotmail.es>
2024-09-08 17:24:55 +0100

    style skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,49 +4,37 @@ import classes from "./Skills.module.css";
 const Skills = () => {
   return (
     <section id="skills">
-      <div className={`${classes.container}`}>
+      <div className={`${classes.container} container`}>
         <hr className={`${classes.dividerTop}`} />
         <div>
-          <ul className={`${classes.skillsList}`}>
+          <ul className={`${classes.skillsList} container`}>
             <li>
               <p className={`${classes.skillName} text-heading-l`}>HTML</p>
-              <span className={`${classes.skillExperience} text-body`}>
-                2 Years Experience
-              </span>
+              <span className="text-body">2 Years Experience</span>
             </li>
             <li>
               <p className={`${classes.skillName} text-heading-l`}>CSS</p>
-              <span className={`${classes.skillExperience} text-body`}>
-                2 Years Experience
-              </span>
+              <span className="text-body">2 Years Experience</span>
             </li>
             <li>
               <p className={`${classes.skillName} text-heading-l`}>
                 Javascript
               </p>
-              <span className={`${classes.skillExperience} text-body`}>
-                2 Years Experience
-              </span>
+              <span className="text-body">2 Years Experience</span>
             </li>
             <li>
               <p className={`${classes.skillName} text-heading-l`}>
                 Typescript
               </p>
-              <span className={`${classes.skillExperience} text-body`}>
-                1 Year Experience
-              </span>
+              <span className="text-body">1 Year Experience</span>
             </li>
             <li>
               <p className={`${classes.skillName} text-heading-l`}>React</p>
-              <span className={`${classes.skillExperience} text-body`}>
-                2 Years Experience
-              </span>
+              <span className="text-body">2 Years Experience</span>
             </li>
             <li>
               <p className={`${classes.skillName} text-heading-l`}>Node</p>
-              <span className={`${classes.skillExperience} text-body`}>
-                2 Years Experience
-              </span>
+              <span className="text-body">2 Years Experience</span>
             </li>
           </ul>
         </div>
