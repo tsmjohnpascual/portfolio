4029d09fdbd9bf71edcc1dfc7a288063d23f50c4
John Pascual <ashleypascual@hotmail.es>
2024-09-10 23:34:17 +0100

    add input error message - needs state

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,11 +3,14 @@ import classes from "./Input.module.css";
 
 const Input = ({ type, placeholder }) => {
   return (
-    <input
-      type={type}
-      className={`${classes.input} text-body`}
-      placeholder={placeholder}
-    />
+    <div>
+      <input
+        type={type}
+        className={`${classes.input} text-body`}
+        placeholder={placeholder}
+      />
+      <span className={`${classes.errorText}`}>Sorry, invalid format here</span>
+    </div>
   );
 };
 
